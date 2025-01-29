3740fdc94cc490ce8a79a8351ea7f6f1996a701d
Dennys28 <66804148+Dennys28@users.noreply.github.com>
2025-01-29 08:04:31 -0500

    Add env.go

---
+++ config/env.go
@@ -0,0 +1,14 @@
+﻿package config
+
+import (
+	"log"
+	"github.com/joho/godotenv"
+)
+
+// loadEnv carga las variables del archivo .env
+func LoadEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error cargando el archivo .env")
+	}
+}
